8ebd374d2e4322cf0992c062a9c792c27c60022f
FaqihAzh <93028236+FaqihAzh@users.noreply.github.com>
2024-01-02 13:07:43 +0700

    Update Promo.js

--- src/redux/actions/Admin/Promo.js
+++ src/redux/actions/Admin/Promo.js
@@ -1,31 +1,44 @@
 import { UpdatePromo, deletePromo } from "../../../services/promo/getPromo"
 import { getPromoId } from "../../../services/promo/getPromoId";
+import { toast } from "react-toastify";
 
 // Delete Oromo
 export const DeletePromoAct = (id) => async () => {
     return await deletePromo(id).then((result) => {
-        alert("berhasil delete Promo")
+        toast("Delete Success!", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: "custom-toast-success",
+      });
         return result
     }).catch((err) => {
-        console.log(err)
+        toast("Delete Failed", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: "custom-toast-error",
+      });
     });
 }
 // update Promo
 export const updatePromoAct = (id, input) => async () => {
     return await UpdatePromo(id, input).then((result) => {
-     alert("Promo Update")
+     toast("Promo Update Success!", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: "custom-toast-success",
+      });
         return result
     }).catch((err) => {
-        console.log(err, 'error')
+        toast(err.response.data.error, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: "custom-toast-error",
+      });
         return err
     });
 }
 // Get Detail Promo
 export const getDetailPromoAct = (id) => async () => {
     return await getPromoId(id).then((result) => {
-        console.log(result.data.data, "detail promo")
+        
         return result.data.data
     }).catch((err) => {
-        console.log(err, "err data promo")
+        return err
     });
 }
